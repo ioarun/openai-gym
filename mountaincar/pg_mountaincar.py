9faf85aca033adfca490993c7fc775f2d7bf1eb5
ioarun <arun_kumar271@hotmail.com>
2018-06-13 12:13:19 +0530

    Mountain Car with Policy Gradient

--- mountaincar/pg_mountaincar.py
+++ mountaincar/pg_mountaincar.py
@@ -1,22 +1,20 @@
 import tensorflow as tf
 import numpy as np
 import gym
-import csv
-
-# writer_file = open('rewards_pg_cartpole.csv', 'wt')
-# writer = csv.writer(writer_file)
-# writer.writerow(['total_rewards'])
+from gym.wrappers import Monitor
 
 EPISODES = 10000
-env = gym.make('CartPole-v0')
+episode_number = 0
+env = gym.make('MountainCar-v0')
+env.seed(1) 
+env = env.unwrapped
 
 # create network graph
 D = env.observation_space.shape[0]
 A = env.action_space.n
 H = 10
-learning_rate = 0.01
-gamma = 0.95
-render = False
+learning_rate = 0.02
+gamma = 0.995
 
 tf.reset_default_graph()
 input_x = tf.placeholder(tf.float32, [None, D], name="input_x")
@@ -35,12 +33,13 @@ fc3 = tf.contrib.layers.fully_connected(inputs = fc2,\
 
 output = tf.nn.softmax(fc3)
 
-input_y = tf.placeholder(tf.float32, [None, 2], name="input_y")
+tvars = tf.trainable_variables()
+input_y = tf.placeholder(tf.float32, [None, 3], name="input_y")
 discounted_rewards = tf.placeholder(tf.float32, name="discounted_rewards")
 neg_log_likelihood = tf.nn.softmax_cross_entropy_with_logits(logits=fc3, labels=input_y)
 product = neg_log_likelihood * discounted_rewards
 loss = tf.reduce_mean(product) # no need for -ve sign if using tf.nn.softmax_cross_entr..... 
-							   # as it gives neg_log_likelihood
+								# as it gives neg_log_likelihood
 
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
@@ -58,32 +57,29 @@ def discounted_rewards_(r):
 def choose_action(output):
 	action = np.random.choice(range(out.shape[1]), p=out.ravel())
 	return action
-# env = gym.wrappers.Monitor(env, 'cartpole-policygradient-monitor/', force=True)
+
+# env = Monitor(env, 'mountain-car-policygradient-monitor/', force=True)
 
 with tf.Session() as sess:
 	sess.run(init)
 
 	xs, drs, ys = [], [], []
-	
-	episode_number = 0
-	reward_sum = 0
-	reward_sum_buffer = []
-	current_state = env.reset()
 
+	reward_sum = 0
+	current_state = env.reset()
+	render = False
 	done = False
 	goal_reached = False
+
 	while not goal_reached:
 		x = np.reshape(current_state, [1, D])
 		out = sess.run(output, feed_dict={input_x: x})
 		action = choose_action(out)
 		xs.append(x)
-		temp_y = np.zeros(2)
+		temp_y = np.zeros(A)
 		temp_y[action] = 1
 		ys.append(temp_y)
-
 		next_state, reward, done, _ = env.step(action)
-		# if render:
-		# 	env.render()
 		drs.append(reward)
 		reward_sum += reward
 
@@ -101,28 +97,18 @@ with tf.Session() as sess:
 		
 			xs, ys, drs = [], [], []
 
-			sess.run(optimizer,  feed_dict={discounted_rewards: discounted_rs, input_x: epx, input_y: epy})
-			
-			reward_sum_buffer.append(reward_sum)
-			if episode_number % 100 == 0:
-				average_per_100_eps = sum(reward_sum_buffer)/100
-				if average_per_100_eps == 200.00: # acieved the goal.
-					goal_reached = True 
+			sess.run([loss, optimizer],  feed_dict={discounted_rewards: discounted_rs, input_x: epx, input_y: epy})
+			print "Reward in episode :", episode_number, "is :", reward_sum
 
-				print "Average reward for ", episode_number," episodes is :", average_per_100_eps
-				reward_sum_buffer = []
-				# writer.writerow([average_per_100_eps])
+			writer.writerow([reward_sum])
 
-			if reward_sum == 200.0:
-				render = True
 			reward_sum = 0
+			
 			current_state = env.reset()
-		
+
 		current_state = next_state
 
-# env.monitor.close()
-
-
+env.close()
 
 			
 			
